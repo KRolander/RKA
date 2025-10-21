1c2ae610f53e694db82a5a9406657c48a1b3acbc
KRolander <kromesrolandgyula@gmail.com>
2025-10-21 11:38:20 +0200

    Added EC-KEM and EC-DEKEM with edwards25519 curve - this curve allows secure elliptic curve arithmetics

--- ec_kem/ec_kem.go
+++ ec_kem/ec_kem.go
@@ -16,6 +16,8 @@ import (
 	"github.com/cloudflare/circl/dh/x25519"
 	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/hkdf"
+
+	"filippo.io/edwards25519"
 )
 
 // derived from kyber/encrypt/ecies.go
@@ -91,9 +93,8 @@ func EC_dekem(c elliptic.Curve, q []byte, C []byte) []byte {
 	return K
 }
 
-// EC-KEM with x25519 curve - which is a safe curve, other arithmetics of "crypto/elliptic"
+// EC-DEKEM with x25519 curve - which is a safe curve, other arithmetics of "crypto/elliptic"
 // cannot be computed safely - DEPRICATED !
-
 // q is the recipient private key
 // C is the encapsulated key
 // returns K the initially encapsulated secret key used for data exchange
@@ -140,4 +141,79 @@ func GenKeyPair_curve25519() (x25519.Key, x25519.Key) {
 	x25519.KeyGen(&pub, &priv)
 
 	return priv, pub
+}
+
+func GenerateKeyPair_edwards25519() ([]byte, []byte, error) {
+	var privKeyBytes [32]byte
+	_, _ = io.ReadFull(rand.Reader, privKeyBytes[:])
+
+	privateKey, err := new(edwards25519.Scalar).SetBytesWithClamping(privKeyBytes[:])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// pub = privateKey * B, where B is the base point
+	pub := new(edwards25519.Point).ScalarBaseMult(privateKey)
+
+	// Canonical 32-byte encoding of v
+	pubKey := pub.Bytes()
+
+	return privateKey.Bytes(), pubKey, err
+
+}
+
+// EC-KEM with x25519 curve - which is a safe curve, other arithmetics of "crypto/elliptic"
+// cannot be computed safely - DEPRICATED !
+// In the original ecies-kem scheme r is generated in the scheme (randomly picked up)
+// In this version the r is an argument (r must be random)
+// Q is the recipient's public key
+// C is the encapsulated key -> (r), the recipiet (Q, q) can compute K = q * C -> q * (r * G) -> K = r*Q
+// K used as secret key for data encryption
+func EC_KEM_edwards25519(r []byte, Q []byte) ([]byte, []byte, error) {
+
+	r_scalar, err := new(edwards25519.Scalar).SetCanonicalBytes(r)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error in SetCanonicalBytes - %v", err)
+	}
+
+	C_point := new(edwards25519.Point).ScalarBaseMult(r_scalar)
+
+	C := C_point.Bytes()
+
+	Q_point, err := new(edwards25519.Point).SetBytes(Q)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error in SetBytes - %v", err)
+	}
+
+	SK_point := new(edwards25519.Point).ScalarMult(r_scalar, Q_point)
+
+	SK := SK_point.Bytes()
+
+	return C, SK, nil
+}
+
+// EC-DEKEM with x25519 curve - which is a safe curve, other arithmetics of "crypto/elliptic"
+// cannot be computed safely - DEPRICATED !
+// q is the recipient private key
+// C is the encapsulated key
+// returns K the initially encapsulated secret key used for data exchange
+// K = q * C -> K = q * (r*G) -> K = r*(q*G) -> K = r * Q
+func EC_DEKEM_edwards25519(q []byte, C []byte) ([]byte, error) {
+
+	q_scalar, err := new(edwards25519.Scalar).SetCanonicalBytes(q)
+	if err != nil {
+		return nil, fmt.Errorf("error in SetCanonicalBytes - %v", err)
+	}
+
+	C_point, err := new(edwards25519.Point).SetBytes(C)
+	if err != nil {
+		return nil, fmt.Errorf("error in SetBytes - %v", err)
+	}
+
+	SK_point := new(edwards25519.Point).ScalarMult(q_scalar, C_point)
+
+	SK := SK_point.Bytes()
+
+	return SK, nil
+
 }
--- ec_kem/ec_kem_test.go
+++ ec_kem/ec_kem_test.go
@@ -6,6 +6,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"testing"
+
+	"filippo.io/edwards25519"
 )
 
 // Test of KEM and DEKEM with x25519 curve - which is a safe curve, other arithmetics of "crypto/elliptic"
@@ -36,4 +38,39 @@ func TestEC_KEM_DEKEM(t *testing.T) {
 		t.Errorf("Error: %v\n", err)
 	}
 	fmt.Printf("_SK: %x\n", _SK)
+}
+
+func TestEC_KEM_DEKEM_ED25519(t *testing.T) {
+	priv, pub, err := GenerateKeyPair_edwards25519()
+	if err != nil {
+		t.Errorf("Error: %v\n", err)
+	}
+
+	fmt.Printf("Private key: %x\n", priv)
+	fmt.Printf("Public key: %x\n", pub)
+
+	r_Bytes, _ := hex.DecodeString("fefcb63981a0ead2fcae71c63c7ea4917b67f57db1b79bb109bf862da35975d7")
+
+	r_Scalar, err := new(edwards25519.Scalar).SetBytesWithClamping(r_Bytes)
+	if err != nil {
+		t.Errorf("Error: %v\n", err)
+	}
+
+	r := r_Scalar.Bytes()
+
+	C, SK, err := EC_KEM_edwards25519(r, pub)
+	if err != nil {
+		t.Errorf("Error: %v\n", err)
+	}
+
+	fmt.Printf("C: %x\n", C)
+	fmt.Printf("SK: %x\n", SK)
+
+	_SK, err := EC_DEKEM_edwards25519(priv, C)
+	if err != nil {
+		t.Errorf("Error: %v\n", err)
+	} else {
+		fmt.Printf("_SK: %x\n", _SK)
+	}
+
 }
